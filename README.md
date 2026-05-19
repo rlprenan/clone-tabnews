f31d71040925fd144c96fcb93c6c10bb1ecb861f
rlprenan <150262456+rlprenan@users.noreply.github.com>
2026-05-19 18:11:17 +0000

    adicionado scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
-function Home () {
-    return <h1>hahaha3</h1>
+function Home() {
+  return <h1>hahaha3</h1>;
 }
-    
+
+function teste() {
+  console.log = "(teste)";
+}
+
+function teste2() {
+  console.log("identacao errada");
+}
+
 export default Home;
